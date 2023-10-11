55779d256408a0b48f5bd81a2ff2770b68dfbcae
Shannon <shannon.treadway@gmail.com>
2023-10-11 10:16:40 -0500

    set up player name inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,59 @@
 import styles from './page.module.css'
+import { PlayerInput } from '@/components/PlayerInput/PlayerInput'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h1>Welcome to Shannon's Scrabble Scoring Website!</h1>
       <div className={styles.description}>
-        <h1>Welcome to Shannon's Scrabble Scoring Website!</h1>          
+        <p>
+        This page is to assist in scoring your Scrabble game. Here are some useful
+        instructions:
+        </p>
+        <ul>
+          <li>
+            There is space for four players. Enter each player's name in the boxes
+            provided.
+          </li>
+          <li>
+            Each player has a place to input words, words to be scored, and a
+            checkbox.
+          </li>
+          <li>
+            The <u>checkbox</u> is for if you use all 7 tiles in your turn. When
+            this happens, check the box and you will receive a 75 point bonus.
+          </li>
+          <li>
+            The <u>your word</u> box is for you to enter the word as you would like
+            it to appear in the table of plays below. You may use any character you
+            wish for blank tiles.
+          </li>
+          <li>
+            The <u>word to score</u> box is for you to input the words taking into
+            account bonus tiles. For example, if you play the word "bat" and the "b"
+            is on the double letter square, type "bbat." In this box, SKIP blank
+            tiles.
+          </li>
+          <li>
+            Along the same lines, if you play a tile on a double or triple word
+            square, repeat the word that many times. For example, if you play the
+            word "bat" and any of the tiles land on a double word square, type
+            "batbat."
+          </li>
+          <li>
+            At times, you might end up playing tiles in such a way that you are
+            forming multiple words. For instance, you might play a "b" before the
+            word "at" to form "bat" then continue your play to form the word "brad."
+            In such instances, you can either play the words as different words
+            ("bat" AND "brad") or all as 1 word ("batbrad").
+          </li>
+          <li>
+            After playing the game, if you are interested in playing another one
+            immediately, refresh the page and it will clear the page.
+          </li>
+        </ul>
       </div>
+      <PlayerInput />
     </main>
   )
 }
---
+++ src/components/PlayerInput/PlayerInput.tsx
@@ -0,0 +1,39 @@
+"use client";
+import styles from './PlayerInput.module.css'
+import { useState } from 'react'
+
+export const PlayerInput = () => {
+    const [player1, setPlayer1] = useState('');
+    const [player2, setPlayer2] = useState('');
+    const [player3, setPlayer3] = useState('');
+    const [player4, setPlayer4] = useState('');
+
+    // const updatePlayerNames = () => {
+    //     setPlayer1(name1);
+    //     setPlayer2(name2);
+    //     setPlayer3(name3);
+    //     setPlayer4(name4);
+    // };
+
+    return (
+        <>
+            <div className={styles.playerInput}>
+                <label for='name1'>Player 1</label>
+                <input name='name1' id='name1' />
+            </div>
+            <div className={styles.playerInput}>
+                <label for='name2'>Player 2</label>
+                <input name='name2' id='name2' />
+            </div>
+            <div className={styles.playerInput}>
+                <label for='name3'>Player 3</label>
+                <input name='name3' id='name3'/>
+            </div>
+            <div className={styles.playerInput}>
+                <label for='name4'>Player 4</label>
+                <input name='name4' id='name4' />
+            </div>
+            {/* <button type='submit' onClick={updatePlayerNames}>Start Game</button> */}
+        </>
+    )
+}
