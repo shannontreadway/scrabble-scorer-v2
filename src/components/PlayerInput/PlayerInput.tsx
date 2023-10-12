b1f66202a347a3f4ae1b6fad43dc83e4b85b182e
Shannon <shannon.treadway@gmail.com>
2023-10-11 22:30:30 -0500

    minor fixes to get a successful build and remove unneeded gitkeep files

--- src/components/PlayerInput/PlayerInput.tsx
+++ src/components/PlayerInput/PlayerInput.tsx
@@ -1,26 +1,27 @@
 import styles from './PlayerInput.module.css'
 
-export const PlayerInput = (setNumber) => {
-    const onClick = (value: number) => {
-        setNumber(value);
-    }
+export const PlayerInput = () => {
+    // setNumber might need to be added as a parameter to this function - was removed to try to get a successful build.
+    // const onClick = (value: number) => {
+    //     setNumber(value);
+    // }
     return (
         <>
             <p className={styles.p}>How many people will be playing the game?</p>
             <div className={styles.form}>
                 <div>
                     <input type="radio" id="2" name="num_players" value={2} />
-                    <label for="2" className={styles.label}>Two</label>
+                    <label htmlFor="2" className={styles.label}>Two</label>
                 </div>
                 <div>
                     <input type="radio" id="3" name="num_players" value={3} />
-                    <label for="3" className={styles.label}>Three</label>
+                    <label htmlFor="3" className={styles.label}>Three</label>
                 </div>
                 <div>
                     <input type="radio" id="4" name="num_players" value={4} />
-                    <label for="4" className={styles.label}>Four</label>
+                    <label htmlFor="4" className={styles.label}>Four</label>
                 </div>
-                <button onClick={onClick} className={styles.submit}>Start Game</button>
+                <button className={styles.submit}>Start Game</button>
             </div>
         </>
       );
